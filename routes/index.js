c3e1e7195bbb295e13ae688dac8cc4a69681d1b0
Abhishek Lalla <abhisheklala1995@gmail.com>
2017-11-14 11:03:43 -0800

    -

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,6 @@ var controllerMongoCollection = require('../controllers/database'); //load contr
 //**************************************************************************
 //***** mongodb get all of the Routes in Routes collection w
 //      and Render information iwith an ejs view
-router.get('/getAllRoutes', controllerMongoCollection.getAllRoutes);
+router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
 
 
