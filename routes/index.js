142bb08011dd252c8ff3c3eea053f5dba2555bfb
Abhishek Lalla <abhisheklala1995@gmail.com>
2017-11-13 19:50:22 -0800

    -

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 module.exports = router;
 
 //LOAD the various controllers
-var controllerMain = require('../controllers/main');   //this will load the main controller file
+//var controllerMain = require('../controllers/database');   //this will load the main controller file
 var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb and Routes collection
 
 //########################################
