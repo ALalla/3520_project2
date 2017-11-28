43011c9963a3d844ee92ba78b0d179a7f6fe6eb7
Abhishek Lalla <abhisheklala1995@gmail.com>
2017-11-27 21:55:27 -0800

    -

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ var controllerMongoCollection = require('../controllers/database'); //load contr
 
 //MAY HAVE OTHER CODE in index.js
 
+var path = require ('path'); //to work with separtors on any OS including Windows
+var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
+
+router.use(bodyParser.json()); // for parsing application/json
+router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
 
 //CODE to route /getAllRoutes to appropriate  Controller function
 //***********************************************
@@ -15,4 +20,4 @@ var controllerMongoCollection = require('../controllers/database'); //load contr
 //      and Render information iwith an ejs view
 //app.post("/storeData', controllerMongoCollection.getAllOrders);
 
-router.post("/storeData", controllerMongoCollection.storeData);
+router.post('/storeData', controllerMongoCollection.storeData);
