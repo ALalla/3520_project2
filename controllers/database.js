e5199de6a52c8c3c1d1b65e914b54adcf912469e
Abhishek Lalla <abhisheklala1995@gmail.com>
2017-11-30 10:13:59 -0800

    -

--- controllers/database.js
+++ controllers/database.js
@@ -11,30 +11,30 @@ router.use(bodyParser.urlencoded({extended: true})); // for parsing application/
 
 module.exports.storeData = function (req, res, next) {
     // setting POST variables
-    /*var firstname = req.body.FIRSTNAME;
+    //userinfo
+    var firstname = req.body.FIRSTNAME;
     var lastname = req.body.LASTNAME;
     var street = req.body.STREET;
     var city = req.body.CITY;
     var state = req.body.STATE;
     var zip = req.body.ZIP;
     var email = req.body.EMAIL;
-
+    //payment
     var creditcardname = req.body.CREDITCARDNAME;
     var creditcardtype = req.body.CREDITCARDTYPE;
     var creditcardnum = req.body.CREDITCARDNUM;
     var creditcardexp = req.body.CREDITCARDEXP;
-
+    //shipping
     var shipping_street = req.body.SHIPPING_STREET;
     var shipping_city = req.body.SHIPPING_CITY;
     var shipping_state = req.body.SHIPPING_STATE;
-    var shipping_zip = req.body.SHIPPING_ZIP;*/
-
+    var shipping_zip = req.body.SHIPPING_ZIP;
+    //order
     var order = req.body.ORDER;
 
 
-    //connect to MONGODB
+    //open mongodb connection
     mongodb.MongoClient.connect(mongoDBURI, function (err, db) {
-        //console.log(mongodb.MongoClient.connect);
         if (err) throw err;
         //CUSTOM IDS
         var customerID = Math.floor((Math.random() * 1000000000000) + 1);
@@ -45,48 +45,48 @@ module.exports.storeData = function (req, res, next) {
         //set POST variables to index
         var customerdata = {
             _id: customerID,
-            FIRSTNAME: req.body.FIRSTNAME,
-            LASTNAME: req.body.LASTNAME,
-            STREET: req.body.STREET,
-            CITY: req.body.CITY,
-            STATE: req.body.STATE,
-            ZIP: req.body.ZIP,
-            EMAIL: req.body.EMAIL
+            FIRSTNAME: firstname,
+            LASTNAME: lastname,
+            STREET: street,
+            CITY: city,
+            STATE: state,
+            ZIP: zip,
+            EMAIL: email
         };
-        //insert Document
+        //insert
         CUSTOMERS.insertOne(customerdata, function (err, result) {
             if (err) throw err;
         });
-        //billing collection operation
+        //billing collection
         var BILLING = db.collection('BILLING');
         //set POST variables to index
         var billingdata = {
             _id: billingID,
-            CREDITCARDNAME: req.body.CREDITCARDNAME,
-            CREDITCARDTYPE: req.body.CREDITCARDTYPE,
-            CREDITCARDNUM: req.body.CREDITCARDNUM,
-            CREDITCARDEXP: req.body.CREDITCARDEXP
+            CREDITCARDNAME: creditcardname,
+            CREDITCARDTYPE: creditcardtype,
+            CREDITCARDNUM: creditcardnum,
+            CREDITCARDEXP: creditcardexp
         };
         //insert Document
         BILLING.insertOne(billingdata, function (err, result) {
             if (err) throw err;
         });
-        //shipping collection operation
+        //shipping collection
         var SHIPPING = db.collection('SHIPPING');
         //set POST variables to index
         var shippingdata = {
             _id: shippingID,
-            SHIPPING_STREET: req.body.SHIPPING_STREET,
-            SHIPPING_CITY: req.body.CITY,
-            SHIPPING_STATE: req.body.SHIPPING_STATE,
-            SHIPPING_ZIP: req.body.SHIPPING_ZIP
+            SHIPPING_STREET: shipping_street,
+            SHIPPING_CITY: shipping_city,
+            SHIPPING_STATE: shipping_state,
+            SHIPPING_ZIP: shipping_zip
         };
         //insert Document
         SHIPPING.insertOne(shippingdata, function (err, result) {
             if (err) throw err;
 
         });
-        //orders collection operation
+        //orders collection
         var ORDERS = db.collection('ORDERS');
         //set POST variables to index
         var order = {
